e55fade5c43a8cd4a8204b3ab0fdcc9cff345c3f
wilie247 <wilsonfemila@gmail.com>
2021-10-06 02:19:02 -0500

    codes

--- call_generic_random_forests.m
+++ call_generic_random_forests.m
@@ -1,35 +1,5 @@
 function call_generic_random_forests()
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Name - call_generic_random_forests
-% Creation Date - 7th July 2015
-% Author - Soumya Banerjee
-% Website - https://sites.google.com/site/neelsoumya/
-%
-% Description - Function to load data and call generic random forests function
-%
-% Parameters - 
-%	Input	
-%
-%	Output
-%               BaggedEnsemble - ensemble of random forests
-%               Plots of out of bag error
-%		Example prediction	
-%
-% Example -
-%		call_generic_random_forests()
-%
-% Acknowledgements -
-%           Dedicated to my mother Kalyani Banerjee, my father Tarakeswar Banerjee
-%				and my wife Joyeeta Ghose.
-%
-% License - BSD
-%
-% Change History - 
-%                   7th July 2015 - Creation by Soumya Banerjee
-%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
 
 load fisheriris
 X = meas;
